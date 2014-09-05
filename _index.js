e5b45be57948c8b3a60239aaf4b70813f805dbc2
Enilia <florian.olivierandre@gmail.com>
2014-09-05 15:39:41 +0200

    real index.js created

---
+++ _index.js
@@ -0,0 +1,49 @@
+var Reporter = require("./Reporters/simpleReporter.reporter"),
+	theMonk = require("./"),
+	path = require("path"),
+	fs = require("fs");
+
+function sim(stats, rotation, time, reporter) {
+
+	stats = JSON.parse(fs.readFileSync(path.resolve(__dirname, "Rotations", stats + ".stats.json"), "utf8"));
+	rotation = fs.readFileSync(path.resolve(__dirname, "Rotations", rotation + ".rotation.js"), "utf8");
+
+	(new theMonk).addActor("Monk", "Monk", stats, rotation)
+				 .setReporter(reporter)
+				 .setMaxTime(time)
+				 .run()
+				 .report(
+						Reporter.prototype.reportResume
+						| Reporter.prototype.reportRotation
+						| Reporter.prototype.reportSkill
+						// | Reporter.prototype.reportAutoAttack
+						// | Reporter.prototype.reportDoT
+						| Reporter.prototype.reportDamage
+			 	 );
+}
+
+var time = 60*3;
+
+console.log("");
+console.log("=== MNK TELRAL (valkky) ===");
+sim("Telral", "monk_2", time, Reporter);
+
+// console.log("");
+// console.log("=== MNK ENILIA (valkky) ===");
+// sim("Enilia", "monk_2", time, Reporter);
+
+// console.log("");
+// console.log("=== MNK 110 DTR ===");
+// sim("MNK_110_DTR", "monk_2", time, Reporter);
+
+// console.log("");
+// console.log("=== MNK 110 CC ===");
+// sim("MNK_110_CC", "monk_2", time, Reporter);
+
+// console.log("");
+// console.log("=== MNK 115 DTR ===");
+// sim("MNK_115_DTR", "monk_2", time, Reporter);
+
+// console.log("");
+// console.log("=== MNK 115 CC ===");
+// sim("MNK_115_CC", "monk_2", time, Reporter);
