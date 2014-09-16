f0a72467366a1da06283647b959d2ad0003d116b
Enilia <florian.olivierandre@gmail.com>
2014-09-16 04:27:58 +0200

    add rotation for Dragoon
    add rotation for Dragoon
    add stats for drg deter BiS

---
+++ Rotations/drg.rotation.js
@@ -0,0 +1,49 @@
+
+if(IsOffGCD()) {
+	if(IsReady("FullThrust") && IsReady("LifeSurge"))
+		return "LifeSurge";
+
+	if(IsReady("InternalRelease"))
+		return "InternalRelease";
+
+	if(IsReady("BloodForBlood"))
+		return "BloodForBlood";
+
+	if(AuraTimeRemaining(target, "Disembowel", self)) {
+		if(IsReady("PowerSurge"))
+			if(IsReady("Jump") || IsReady("SpineshatterDive"))
+				return "PowerSurge";
+
+		if(IsReady("Jump"))
+			return "Jump";
+		if(IsReady("SpineshatterDive"))
+			return "SpineshatterDive";
+	}
+}
+
+if(IsReady("VorpalThrust")) {
+	return "VorpalThrust";
+}
+
+if(IsReady("FullThrust")) {
+	return "FullThrust";
+}
+
+if(IsReady("Disembowel")) {
+	return "Disembowel";
+}
+
+if(IsReady("ChaosThrust")) {
+	return "ChaosThrust";
+}
+
+if(AuraTimeRemaining(self, "HeavyThrust", self) < 4)
+	return "HeavyThrust";
+
+if(AuraTimeRemaining(target, "PhlebotomizeDOT", self) < 4)
+	return "Phlebotomize";
+
+if(AuraTimeRemaining(target, "ChaosThrustDOT", self) < GCD() * 3)
+	return "ImpulseDrive";
+
+return "TrueThrust";
