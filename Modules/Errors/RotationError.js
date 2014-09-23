e6f9e6a9c1e82b2d72a071e3d0fc81a0d9ba1e34
Enilia <florian.olivierandre@gmail.com>
2014-09-23 08:45:05 +0200

    Cleaned RotationError.js
    constructor arguments name changed
    #message is now a getter
    #getSourceErrorPosition: removed unnecessary string from regexp
    defined #toString in the prototype (this.toString was throwing an
    exception)

--- Modules/Errors/RotationError.js
+++ Modules/Errors/RotationError.js
@@ -4,10 +4,9 @@ var inherits = require("util").inherits,
 
 exports.RotationError = RotationError;
 
-function RotationError(error, arguments, source) {
-	this.message = error.message;
+function RotationError(error, args, source) {
 	this.error = error;
-	this.arguments = arguments;
+	this.arguments = args;
 	this.source = source;
 }
 
@@ -20,33 +19,47 @@ extend(RotationError.prototype, {
 	error: null,
 
 	getSourceErrorPosition: function() {
-		var m = this.error.stack.match(/at undefined:(\d+):(\d+)/);
+		var m = this.error.stack.match(/:(\d+):(\d+)/);
 
 		return {
 			line: parseInt(m[1]) - 1,
 			col: parseInt(m[2])
 		};
 	},
+
+	toString: function() {
+		return this.name + ": " + this.message;
+	},
 });
 
-Object.defineProperty(RotationError.prototype, "stack", {
-	configurable: true,
-	enumerable: true,
-	get: function() {
-		var pos = this.getSourceErrorPosition(),
-			source = this.source.split(/\r?\n/),
-			begin = Math.max(pos.line - 1 - 1, 0),
-			end = Math.min(pos.line + 1 - 1, source.length - 1),
-			stack = "",
-			i, isFaultyLine;
+Object.defineProperties(RotationError.prototype, {
+	"stack": {
+		configurable: true,
+		enumerable: true,
+		get: function() {
+			var pos = this.getSourceErrorPosition(),
+				source = this.source.split(/\r?\n/),
+				begin = Math.max(pos.line - 1 - 1, 0),
+				end = Math.min(pos.line + 1 - 1, source.length - 1),
+				stack = "",
+				i, isFaultyLine;
 
-		stack += format("[%s] at line %d, col %d\n\n", this.toString(), pos.line, pos.col);
-		for(; begin <= end; begin++) {
-			isFaultyLine = (begin === pos.line - 1);
-			stack += format("%s%s\n", (begin + 1 + "   ").slice(0,3), source[begin].replace(/^\t/, " "));
-			isFaultyLine && (stack += format("---%s^\n", Array(pos.col).join("-")));
+			stack += format("\n[%s] at line %d, col %d\n\n", this.toString(), pos.line, pos.col);
+			for(; begin <= end; begin++) {
+				isFaultyLine = (begin === pos.line - 1);
+				stack += format("%s%s\n", (begin + 1 + "   ").slice(0,3), source[begin].replace(/\t/g, " "));
+				isFaultyLine && (stack += format("---%s^\n", Array(pos.col).join("-")));
+			}
+
+			return stack;
 		}
+	},
 
-		return stack;
+	"message": {
+		configurable: true,
+		enumerable: true,
+		get: function() {
+			return this.error.message;
+		}
 	}
-})
+});
