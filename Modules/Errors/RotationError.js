f170763a88417ee93243c041f5bd1b6b318aec4f
Enilia <florian.olivierandre@gmail.com>
2014-09-25 16:21:40 +0200

    Fixed custom errors stacktrace capture

--- Modules/Errors/RotationError.js
+++ Modules/Errors/RotationError.js
@@ -8,6 +8,8 @@ function RotationError(error, args, source) {
 	this.error = error;
 	this.arguments = args;
 	this.source = source;
+	
+	captureStackTrace(this);
 }
 
 inherits(RotationError, Error);
@@ -26,40 +28,30 @@ extend(RotationError.prototype, {
 			col: parseInt(m[2])
 		};
 	},
-
-	toString: function() {
-		return this.name + ": " + this.message;
-	},
 });
 
-Object.defineProperties(RotationError.prototype, {
-	"stack": {
-		configurable: true,
-		enumerable: true,
-		get: function() {
-			var pos = this.getSourceErrorPosition(),
-				source = this.source.split(/\r?\n/),
-				begin = Math.max(pos.line - 1 - 1, 0),
-				end = Math.min(pos.line + 1 - 1, source.length - 1),
-				stack = "",
-				i, isFaultyLine;
+function captureStackTrace(rotationError) {
+	Object.defineProperties(rotationError, {
+		"stack": {
+			configurable: true,
+			enumerable: true,
+			get: function() {
+				var pos = this.getSourceErrorPosition(),
+					source = this.source.split(/\r?\n/),
+					begin = Math.max(pos.line - 1 - 1, 0),
+					end = Math.min(pos.line + 1 - 1, source.length - 1),
+					stack = "",
+					i, isFaultyLine;
 
-			stack += format("\n[%s] at line %d, col %d\n\n", this.toString(), pos.line, pos.col);
-			for(; begin <= end; begin++) {
-				isFaultyLine = (begin === pos.line - 1);
-				stack += format("%s%s\n", (begin + 1 + "   ").slice(0,3), source[begin].replace(/\t/g, " "));
-				isFaultyLine && (stack += format("---%s^\n", Array(pos.col).join("-")));
+				stack += format("\n[%s] at line %d, col %d\n\n", this.toString(), pos.line, pos.col);
+				for(; begin <= end; begin++) {
+					isFaultyLine = (begin === pos.line - 1);
+					stack += format("%s%s\n", (begin + 1 + "   ").slice(0,3), source[begin].replace(/\t/g, " "));
+					isFaultyLine && (stack += format("---%s^\n", Array(pos.col).join("-")));
+				}
+
+				return stack;
 			}
-
-			return stack;
-		}
-	},
-
-	"message": {
-		configurable: true,
-		enumerable: true,
-		get: function() {
-			return this.error.message;
-		}
-	}
-});
+		},
+	});
+}
--- Modules/Errors/RotationSyntaxError.js
+++ Modules/Errors/RotationSyntaxError.js
@@ -5,9 +5,12 @@ var inherits = require("util").inherits,
 exports.RotationSyntaxError = RotationSyntaxError;
 
 function RotationSyntaxError(error, args, source) {
+	this.message = error.message;
 	this.error = error;
 	this.arguments = args;
 	this.source = source;
+
+	captureStackTrace(this);
 }
 
 inherits(RotationSyntaxError, Error);
@@ -17,52 +20,42 @@ extend(RotationSyntaxError.prototype, {
 	arguments: null,
 	source: null,
 	error: null,
-
-	toString: function() {
-		return this.name + ": " + this.message;
-	},
 });
 
-Object.defineProperties(RotationSyntaxError.prototype, {
-	"stack": {
-		configurable: true,
-		enumerable: true,
-		get: function() {
-			var source = this.source.split(/\r?\n/),
-				token = this.error.arguments[0],
-				stack = "",
-				index = 0, length = source.length,
-				line, col,
-				begin, end, isFaultyLine;
+function captureStackTrace(rotationSyntaxError) {
+	Object.defineProperties(rotationSyntaxError, {
+		"stack": {
+			configurable: true,
+			enumerable: true,
+			get: function() {
+				var source = this.source.split(/\r?\n/),
+					token = this.error.arguments[0],
+					stack = "",
+					index = 0, length = source.length,
+					line, col,
+					begin, end, isFaultyLine;
 
-			stack += format("\n[%s]\n\n", this.toString());
-			stack += "Possible error locations:\n\n";
+				stack += format("\n %s\n\n", this.toString());
+				stack += "Possible error locations:\n\n";
 
-			for(; index < length; index++) {
-				line = source[index];
-				if(~(col = line.indexOf(token))) {
-					begin = Math.max(index - 1, 0);
-					end = Math.min(index + 1, source.length - 1);
+				for(; index < length; index++) {
+					line = source[index];
+					if(~(col = line.indexOf(token))) {
+						begin = Math.max(index - 1, 0);
+						end = Math.min(index + 1, source.length - 1);
 
-					stack += format("@%d:%d\n", index + 1, col + 1);
-					for(; begin <= end; begin++) {
-						isFaultyLine = (begin === index);
-						stack += format("%s%s\n", (begin + 1 + "   ").slice(0,3), source[begin].replace(/\t/g, " "));
-						isFaultyLine && (stack += format("   %s^\n", Array(col + 1).join(" ")));
+						stack += format("@%d:%d\n", index + 1, col + 1);
+						for(; begin <= end; begin++) {
+							isFaultyLine = (begin === index);
+							stack += format("%s%s\n", (begin + 1 + "   ").slice(0,3), source[begin].replace(/\t/g, " "));
+							isFaultyLine && (stack += format("   %s^\n", Array(col + 1).join(" ")));
+						}
+						stack += "\n";
 					}
-					stack += "\n";
 				}
+
+				return stack;
 			}
-
-			return stack;
-		}
-	},
-
-	"message": {
-		configurable: true,
-		enumerable: true,
-		get: function() {
-			return this.error.message;
-		}
-	}
-});
+		},
+	});
+}
