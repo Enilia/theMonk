a2c534ff2b159d819ff0fe941820b42a2dfaa884
Enilia <florian.olivierandre@gmail.com>
2015-09-13 02:56:34 +0200

    Hooked themonk on form submit

--- browser/js/controllers/actorController.js
+++ browser/js/controllers/actorController.js
@@ -32,7 +32,7 @@ angular.module('themonkControllers', [])
 		};
 
 		$scope.addSkill = function(skill) {
-			$scope.rotation = $scope.rotation + skill.name;
+			$scope.rotation = $scope.rotation + '"' + skill.name + '"';
 		};
 
 		$scope.addVar = function(variable, $event) {
@@ -43,6 +43,33 @@ angular.module('themonkControllers', [])
 		$scope.addFn = function(fn, args, $event) {
 			$event.preventDefault();
 			$scope.rotation = $scope.rotation + fn + "(" + args.join(", ") + ")";
+		};
+
+		$scope.submit = function() {
+			new TheMonk().on("error", function(e) {
+					switch(e.name) {
+						case "RotationError":
+						case "RotationSyntaxError":
+							console.error(e.stack);
+							console.error(e.error.stack);
+							break;
+						default:
+							console.error(e.stack);
+					}
+					throw e;
+				})
+				.on("warn", function(warn) {
+					console.warn(warn);
+				})
+				.addActor($scope.model.name, $scope.name, $scope.stats, $scope.rotation)
+				.setMaxTime(600)
+				.run()
+				.on("progress", function(time, maxTime) {
+					console.log("%d %d", time, maxTime);
+				})
+				.on("end", function(duration) {
+					console.log("done");
+				});
 		};
 
 	}]);
